4cd6f35a0be823b9182ccacd68a95d6805b1f9f3
OliMac1 <olivermcleod96@gmail.com>
2018-03-02 22:13:42 +0000

    small modification to account for specific edge case, may be accounted for by later stages and removed

--- src/main/java/Stages/RowsAndColumnsStage.java
+++ src/main/java/Stages/RowsAndColumnsStage.java
@@ -70,9 +70,24 @@ public class RowsAndColumnsStage {
                     for(int i = 0; i < p.getB(); i++){
                         modified |= board.setTent(col, p.getA() + i);
                     }
+                }else{
+                    for(int i = 0; i < p.getB(); i++){
+                        modified |= board.setTile(col + 1, p.getA() + i, Tile.GRASS);
+                        modified |= board.setTile(col - 1, p.getA() + i, Tile.GRASS);
+                    }
                 }
             }
             return modified;
+        }else if(suggested == total - 1){
+            if(pairs.size() == 1 && pairs.get(0).getB()%2 == 1){
+                boolean modified = false;
+                Pair p = pairs.get(0);
+                for(int i = 1; i < pairs.get(0).getB(); i += 2){
+                    modified |= board.setTile(col + 1, p.getA() + i, Tile.GRASS);
+                    modified |= board.setTile(col - 1, p.getA() + i, Tile.GRASS);
+                }
+                return modified;
+            }
         }
         return false;
     }
@@ -127,9 +142,24 @@ public class RowsAndColumnsStage {
                     for(int i = 0; i < p.getB(); i++){
                         modified |= board.setTent(p.getA() + i,row);
                     }
+                }else{
+                    for(int i = 0; i < p.getB(); i++){
+                        modified |= board.setTile(p.getA() + i, row + 1, Tile.GRASS);
+                        modified |= board.setTile(p.getA() + i, row - 1, Tile.GRASS);
+                    }
                 }
             }
             return modified;
+        }else if(suggested == total - 1){
+            if(pairs.size() == 1 && pairs.get(0).getB()%2 == 1){
+                boolean modified = false;
+                Pair p = pairs.get(0);
+                for(int i = 1; i < pairs.get(0).getB(); i += 2){
+                    modified |= board.setTile(p.getA() + i, row + 1, Tile.GRASS);
+                    modified |= board.setTile(p.getA() + i, row - 1, Tile.GRASS);
+                }
+                return modified;
+            }
         }
         return false;
     }
